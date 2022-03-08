c1b689a4f440b4fb936a235fee42077274d45a8d
Alireza Safaierad <frontendmonster@gmail.com>
2022-03-08 18:59:32 +0400

    improve awesome

---
+++ .config/awesome/modules/menu.lua
@@ -0,0 +1,139 @@
+local awful = require('awful')
+local gears = require('gears')
+local wibox = require('wibox')
+local menubar = require("menubar")
+local hotkeys_popup = require("awful.hotkeys_popup")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local sutils = loadrc('utils')
+
+require("awful.hotkeys_popup.keys")
+
+local my_awesome_menu = {
+  {"hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end},
+  {"manual", terminal .. " -e man awesome"}, {"edit config", editor_cmd .. " " .. awesome.conffile},
+  {"restart", awesome.restart}, {"quit", function() awesome.quit() end}
+}
+
+menubar.utils.terminal = terminal
+
+-- {{{ Wibar
+
+-- Create a wibox for each screen and add it
+local taglist_buttons = gears.table.join(awful.button({}, 1, function(t)
+    t:view_only()
+end), awful.button({}, 3, awful.tag.viewtoggle), awful.button({}, 4, function(t)
+    awful.tag.viewnext(t.screen)
+end), awful.button({}, 5, function(t)
+    awful.tag.viewprev(t.screen)
+end))
+
+local tasklist_buttons = gears.table.join(awful.button({}, 1, function(c)
+    if c == client.focus then
+        c.minimized = true
+    else
+        c:emit_signal("request::activate", "tasklist", {
+            raise = true
+        })
+    end
+end), awful.button({}, 3, function()
+    awful.menu.client_list({
+        theme = {
+            width = 250
+        }
+    })
+end), awful.button({}, 4, focus_next), awful.button({}, 5, focus_prev))
+
+local date = wibox.widget.textclock(" %a %b %d ")
+local time = wibox.widget.textclock("%H:%M", 10)
+
+local tray = wibox.widget.systray()
+tray:set_base_size(12)
+
+function initMenu(s)
+    sutils.set_wallpaper(s)
+    s.padding = {
+        top = dpi(4)
+    }
+    awful.tag({"sh", "web", "dev", "file", "music", "video", "slack", "discord", "tel"}, s, awful.layout.suit.tile)
+
+    s.mypromptbox = awful.widget.prompt()
+
+    s.mytaglist = awful.widget.taglist {
+        screen = s,
+        filter = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons,
+        widget_template = {
+            {
+                {
+                    {
+                        {
+                            id = 'text_role',
+                            widget = wibox.widget.textbox
+                        },
+                        widget = wibox.container.margin
+                    },
+                    layout = wibox.layout.fixed.horizontal
+                },
+                left = dpi(12),
+                right = dpi(12),
+                widget = wibox.container.margin
+            },
+            id = 'background_role',
+            widget = wibox.container.background
+        }
+    }
+
+    s.mytasklist = awful.widget.tasklist {
+        screen = s,
+        filter = awful.widget.tasklist.filter.currenttags,
+        buttons = tasklist_buttons
+    }
+
+    s.mywibox = awful.wibar({
+        position = "top",
+        width = s.geometry.width - dpi(8),
+        height = dpi(35),
+        screen = s,
+        stretch = false,
+        margins = dpi(10)
+    })
+    s.mywibox.y = dpi(4)
+
+    s.mywibox:setup{
+        right = dpi(36),
+        left = dpi(16),
+        layout = wibox.container.margin,
+        {
+            layout = wibox.layout.stack,
+            {
+                layout = wibox.layout.align.horizontal,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    s.mytaglist,
+                    s.mypromptbox
+                },
+                nil,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    spacing = dpi(8),
+                    awful.widget.keyboardlayout(),
+                    date,
+                    {
+                        tray,
+                        valign = "center",
+                        halign = "center",
+                        layout = wibox.container.place
+                    }
+                }
+            },
+            {
+                time,
+                valign = "center",
+                halign = "center",
+                layout = wibox.container.place
+            }
+        }
+    }
+end
+
--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -10,42 +10,34 @@ local theme = dofile(themes_path.."default/theme.lua")
 -- load vector assets' generators for this theme
 
 theme.font          = "sans 8"
+theme.accent        = xrdb.color11
+theme.error         = xrdb.color9
+theme.fg            = xrdb.foreground
+theme.bg            = xrdb.background
+theme.mute          = xrdb.color8
 
-theme.bg_normal     = xrdb.background
-theme.bg_focus      = xrdb.color3
-theme.bg_urgent     = xrdb.color9
-theme.bg_minimize   = xrdb.color8
-theme.bg_systray    = theme.bg_normal
+theme.bg_normal     = theme.bg
+theme.bg_focus      = theme.bg
+theme.bg_urgent     = theme.bg
+theme.bg_minimize   = theme.bg
+theme.bg_systray    = theme.bg
 
-theme.fg_normal     = xrdb.foreground
-theme.fg_focus      = theme.bg_normal
-theme.fg_urgent     = theme.bg_normal
-theme.fg_minimize   = theme.bg_normal
+theme.fg_normal     = theme.fg
+theme.fg_focus      = theme.accent
+theme.fg_urgent     = theme.error
+theme.fg_minimize   = theme.mute
+theme.taglist_fg_empty = theme.mute
+
+theme.border_width  = dpi(2)
+theme.border_normal = theme.accent
+theme.border_focus  = theme.accent
+theme.border_marked = xrdb.color10
 
 theme.useless_gap   = dpi(2)
-theme.border_width  = dpi(5)
-theme.border_normal = theme.bg_normal
-theme.border_focus  = theme.bg_normal
-theme.border_marked = xrdb.color10
 theme.systray_icon_spacing = dpi(6)
-
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
 
 theme.tooltip_fg = theme.fg_normal
 theme.tooltip_bg = theme.bg_normal
-
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
 theme.menu_submenu_icon = themes_path.."default/submenu.png"
 theme.menu_height = dpi(16)
 theme.menu_width  = dpi(100)
