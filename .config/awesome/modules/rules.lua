beffeda541be535eb85d0edea6f5323a7d8067bf
Alireza Safaierad <frontendmonster@gmail.com>
2023-10-18 02:57:37 +0400

    format awesome

--- .config/awesome/modules/rules.lua
+++ .config/awesome/modules/rules.lua
@@ -1,39 +1,56 @@
 local awful = require("awful")
 local keys = require("modules.keys")
 
-local rules = { 
+local rules = {
   {
     rule = {},
     properties = {
-        focus = awful.client.focus.filter,
-        raise = true,
-        keys = keys.client_keys,
-        buttons = keys.client_buttons,
-        screen = awful.screen.preferred,
-        placement = awful.placement.no_overlap + awful.placement.no_offscreen
-    }
+      focus = awful.client.focus.filter,
+      raise = true,
+      keys = keys.client_keys,
+      buttons = keys.client_buttons,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+    },
   },
   {
     rule_any = {
-        instance = { "copyq", "pinentry" },
-        class = { "Arandr", "Blueman-manager", "Pavucontrol", "Gpick", "Kruler", "MessageWin", "Sxiv", "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer", "persepolis", "Gcolor3" },
-        name = { "Event Tester" },
-        role = { "pop-up" }
+      instance = { "copyq", "pinentry" },
+      class = {
+        "Arandr",
+        "Flows",
+        "Blueman-manager",
+        "Pavucontrol",
+        "Gpick",
+        "Kruler",
+        "MessageWin",
+        "Sxiv",
+        "Tor Browser",
+        "Wpa_gui",
+        "veromix",
+        "xtightvncviewer",
+        "persepolis",
+        "Gcolor3",
+      },
+      name = { "Event Tester" },
+      role = { "pop-up" },
     },
     properties = {
-        floating = true,
-        placement = awful.placement.centered,
-        size_hints = { max_height = 300, min_width = 200 }
-    }
+      floating = true,
+      placement = awful.placement.centered,
+      size_hints = { max_height = 300, min_width = 200 },
+    },
   },
   {
     rule = { instance = "flyterm" },
     properties = { floating = true },
-    callback = function(c) c:geometry({ width = 600, height = 400 }) end
+    callback = function(c)
+      c:geometry({ width = 600, height = 400 })
+    end,
   },
   {
     rule = { class = "Brave-browser" },
-    properties = { tag = tags[2] } 
+    properties = { tag = tags[2] },
   },
   {
     rule = { class = "Slack" },
@@ -41,12 +58,12 @@ local rules = {
   },
   {
     rule = { class = "Code" },
-    properties = { tag = tags[3] }
+    properties = { tag = tags[3] },
   },
   {
     rule = { class = "TelegramDesktop" },
-    properties = { tag = tags[9] }
-  }
+    properties = { tag = tags[9] },
+  },
 }
 
 return rules
