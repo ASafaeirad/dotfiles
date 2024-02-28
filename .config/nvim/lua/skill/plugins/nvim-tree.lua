f14c29bee08f35399ec3a88d5e01e9176998e2d1
Alireza Safaierad <frontendmonster@gmail.com>
2024-02-28 01:27:48 +0000

    fix nvim errors

--- .config/nvim/lua/skill/plugins/nvim-tree.lua
+++ .config/nvim/lua/skill/plugins/nvim-tree.lua
@@ -1,77 +1,69 @@
 local M = {}
 
 local function config()
-	local status_ok, nvim_tree = pcall(require, "nvim-tree")
-	if not status_ok then
-		return
-	end
+    local status_ok, nvim_tree = pcall(require, "nvim-tree")
+    if not status_ok then
+        return
+    end
 
-	nvim_tree.setup({
-		hijack_netrw = true,
-		update_focused_file = {
-			enable = true,
-			update_cwd = true,
-		},
-		filters = {
-			dotfiles = false,
-			custom = { "^\\.git" },
-		},
-		git = {
-			enable = true,
-			ignore = true,
-			show_on_dirs = false,
-		},
-		renderer = {
-			root_folder_modifier = ":t",
-			icons = {
-				glyphs = {
-					default = "", -- 
-					symlink = "",
-					folder = {
-						arrow_open = "-", -- 
-						arrow_closed = "+", -- 
-						default = "", -- 
-						open = "", -- 
-						empty = "", -- 
-						empty_open = "", -- 
-						symlink = "",
-						symlink_open = "",
-					},
-					git = {
-						unstaged = "",
-						staged = "S",
-						unmerged = "",
-						renamed = "➜",
-						untracked = "",
-						deleted = "",
-						ignored = "◌",
-					},
-				},
-			},
-		},
-		diagnostics = {
-			enable = true,
-			show_on_dirs = false,
-			icons = {
-				-- hint = "",
-				-- info = "",
-				-- warning = "",
-				-- error = "",
-			},
-		},
-		view = {
-			width = 30,
-			side = "left",
-			mappings = {
-				list = {
-					{ key = { "l", "<CR>", "o" }, action = "edit" },
-					{ key = "h", action = "close_node" },
-					{ key = "v", action = "vsplit" },
-					{ key = "f", cb = function() end },
-				},
-			},
-		},
-	})
+    nvim_tree.setup({
+        hijack_netrw = true,
+        update_focused_file = {
+            enable = true,
+            update_cwd = true
+        },
+        filters = {
+            dotfiles = false,
+            custom = {"^\\.git"}
+        },
+        git = {
+            enable = true,
+            ignore = true,
+            show_on_dirs = false
+        },
+        renderer = {
+            root_folder_modifier = ":t",
+            icons = {
+                glyphs = {
+                    default = "", -- 
+                    symlink = "",
+                    folder = {
+                        arrow_open = "-", -- 
+                        arrow_closed = "+", -- 
+                        default = "", -- 
+                        open = "", -- 
+                        empty = "", -- 
+                        empty_open = "", -- 
+                        symlink = "",
+                        symlink_open = ""
+                    },
+                    git = {
+                        unstaged = "",
+                        staged = "S",
+                        unmerged = "",
+                        renamed = "➜",
+                        untracked = "",
+                        deleted = "",
+                        ignored = "◌"
+                    }
+                }
+            }
+        },
+        diagnostics = {
+            enable = true,
+            show_on_dirs = false,
+            icons = {
+                -- hint = "",
+                -- info = "",
+                -- warning = "",
+                -- error = "",
+            }
+        },
+        view = {
+            width = 30,
+            side = "left"
+        }
+    })
 end
 
 M.config = config
