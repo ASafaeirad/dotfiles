c0d3280fe19307e4102d5e3dccd704cbeaa20edf
Alireza Safaierad <frontendmonster@gmail.com>
2024-02-28 01:27:48 +0000

    fix nvim errors

--- .config/nvim/lua/skill/plugins/indent-blankline.lua
+++ .config/nvim/lua/skill/plugins/indent-blankline.lua
@@ -1,21 +1,21 @@
 local M = {}
 
 local function config()
-	require("indent_blankline").setup({
-		filetype_exclude = {
-			"help",
-			"terminal",
-			"dashboard",
-			"alpha",
-			"packer",
-			"TelescopePrompt",
-			"TelescopeResults",
-			"",
+	require("ibl").setup({
+		exclude = {
+			buftypes = { "terminal", "nofile" },
+			filetypes = {
+				"help",
+				"terminal",
+				"dashboard",
+				"alpha",
+				"packer",
+				"TelescopePrompt",
+				"TelescopeResults",
+				"",
+			},
 		},
-		buftype_exclude = { "terminal", "nofile" },
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = false,
+		scope = { enabled = true, show_start = false },
 	})
 end
 
