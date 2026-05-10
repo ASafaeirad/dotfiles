e5ddadc123396dfbae85e88b186d54336cd7c5fc
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-05-10 23:05:07 +0200

    awesome: remove colors.lua in favor of theme.lua

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -3,14 +3,16 @@ local xresources = require("beautiful.xresources")
 local naughty = require("naughty")
 local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
-local colors = require("modules.colors")
 local themes_path = gfs.get_themes_dir()
+
+local xrdb = xresources.get_current_theme()
 
 local theme = dofile(themes_path .. "default/theme.lua")
 
 theme.font = "Monaspace Argon 10"
-theme.fg = colors.fg
-theme.bg = colors.bg
+theme.fg = xrdb.foreground
+theme.bg = xrdb.background
+theme.bg_alt = "#323643"
 
 theme.bg_normal = theme.bg
 theme.bg_focus = theme.bg
@@ -19,23 +21,23 @@ theme.bg_minimize = theme.bg
 theme.bg_systray = theme.bg
 
 theme.fg_normal = theme.fg
-theme.fg_focus = colors.accent
-theme.fg_urgent = colors.error
-theme.fg_minimize = colors.mute
+theme.fg_focus = xrdb.color11
+theme.fg_urgent = xrdb.color9
+theme.fg_minimize = xrdb.color8
 
-theme.error = colors.error
-theme.accent = colors.accent
-theme.success = colors.success
-theme.mute = colors.mute
+theme.error = xrdb.color9
+theme.accent = xrdb.color11
+theme.success = xrdb.color10
+theme.mute = xrdb.color8
 
 theme.taglist = {
   padding = dpi(12),
 }
-theme.taglist_fg_empty = colors.mute
+theme.taglist_fg_empty = theme.mute
 
 theme.border_width = dpi(10)
-theme.border_normal = colors.accent
-theme.border_focus = colors.accent
+theme.border_normal = theme.accent
+theme.border_focus = theme.accent
 theme.border_marked = "#FF0000"
 
 theme.useless_gap = dpi(2)
@@ -62,10 +64,10 @@ nconf.defaults.timeout = 6
 nconf.defaults.position = "bottom_right"
 nconf.padding = dpi(10)
 nconf.spacing = 8
-nconf.presets.normal.bg = colors.bg1
+nconf.presets.normal.bg = theme.bg_alt
 nconf.defaults.margin = dpi(16)
-nconf.presets.critical.bg = colors.error
-nconf.presets.critical.fg = colors.bg
+nconf.presets.critical.bg = theme.error
+nconf.presets.critical.fg = theme.bg
 
 -- Remove taglist squares:
 local taglist_square_size = dpi(0)
